c81543756791412100f54000e834d5483a891ee7
Rafaella <rafacarvalhosilva11@icloud.com>
2026-05-13 16:24:39 -0300

    Atividade

---
+++ 1.1-exercicio01.py
@@ -0,0 +1,40 @@
+# EX1
+idade = 10 
+print(type(idade))
+
+
+# EX 2
+temperatura = 23.5
+print ("Valor:", temperatura) 
+print ("Tipo:", type(temperatura))
+
+
+# EX 3
+impedância_eletrica = 5 + 8j
+print ("Parte Real:", impedância_eletrica.real)
+
+
+# EX 4
+print ("Parte Imaginária:", impedância_eletrica.imag)
+
+
+# EX 5 
+populacao = 8_000_000_000
+print (type(populacao))
+
+# EX 6
+print(type(7) == int)
+
+
+# EX 7
+aprovado = True 
+print(type(aprovado))
+
+
+# EX 8
+resultado = True + False 
+print(resultado)
+print(type(resultado))
+
+
+# EX 9
